d2023bc6c4620661d7dd7671b4976a35dbb0e3a9
seohong <seokchang_h@naver.com>
2021-08-08 01:36:11 +0900

    Modified todo.js - Added todoList LocalStorage / Delete todoList / Update LocalStorage

--- VanillaJS/js/todo.js
+++ VanillaJS/js/todo.js
@@ -1,11 +1,56 @@
 const todoForm = document.getElementById("todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
+let toDos = [];
+const TODOS_KEY = "toDos";
+
+const localStorageTodo = localStorage.getItem(TODOS_KEY);
+
+if (localStorageTodo !== null) {
+	const parseToDos = JSON.parse(localStorageTodo);
+	toDos = parseToDos;
+	// '=>' : arrow function(ECMAScript6/EC6)
+	parseToDos.forEach(paintToDo);
+}
+
+function paintToDo(newToDoObj) {
+	const li = document.createElement("li");
+	const span = document.createElement("span");
+	const button = document.createElement("button");
+
+	li.id = newToDoObj.id;
+	span.innerText = newToDoObj.text;
+	button.innerText = "❌";
+	button.addEventListener("click", onRemoveToDo);
+	todoList.appendChild(li);
+	li.appendChild(span);
+	li.appendChild(button);
+}
+
+function onRemoveToDo(event) {
+	const li = event.target.parentNode;
+	toDos = toDos.filter(todo => todo.id !== parseInt(li.id));
+	li.remove();
+	onSaveToDo();
+}
+
+function onSaveToDo() {
+	localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
 
 function handlerToDoFormSubmit(event) {
 	event.preventDefault();
 	const newToDo = todoInput.value;
-	todoInput.value = "";	
+	todoInput.value = "";
+
+	const newToDoObj = {
+		id: Date.now(),
+		text: newToDo,
+	}
+
+	toDos.push(newToDoObj);
+	paintToDo(newToDoObj);
+	onSaveToDo();
 }
 
 todoForm.addEventListener("submit", handlerToDoFormSubmit);
